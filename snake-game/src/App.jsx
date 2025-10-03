8a8db5ebb87cba5112b2d677983e9f1215b00bac
karthikeyan <mkksubscribe@gmail.com>
2025-10-03 20:02:31 +0530

    bug fixing

--- snake-game/src/App.jsx
+++ snake-game/src/App.jsx
@@ -1,7 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import "./App.css";
-import { useEffect } from "react";
-import { useRef } from "react";
 import Button from "./components/Button";
 import {
   DownArrow,
